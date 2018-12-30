65f4d7f1297d34be4fa643917fdebe2bf07a11b0
ahmedhhw <ahmedhhw@gmail.com>
2018-12-30 18:00:53 -0500

    First Push, setting up library and Java project

---
+++ Platformer/src/Platformer.java
@@ -0,0 +1,12 @@
+import processing.core.*;
+public class Platformer extends PApplet{
+	public static void main(String[] args) {
+		PApplet.main("Platformer");
+	}
+	public void setup() {
+		ellipse(200, 200, 200, 200);
+	}
+	public void settings() {
+		  size(1920, 1080);
+	}
+}
