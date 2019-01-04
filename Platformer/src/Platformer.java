80f4ceb083437dc1b0778fa30914eefe921ed128
DoubleA <DoubleA@DESKTOP-UD52PQS.fios-router.home>
2019-01-04 16:55:46 -0500

    Cleared up some more files

--- Platformer/src/Platformer.java
+++ Platformer/src/Platformer.java
@@ -2,8 +2,7 @@ import backgroundUtils.Circle;
 import backgroundUtils.Vector;
 import processing.core.*;
 public class Platformer extends PApplet{
-	private Circle ball;
-	private double gravity;
+	
 	public static void main(String[] args) {
 		PApplet.main("Platformer");
 	}
@@ -11,8 +10,6 @@ public class Platformer extends PApplet{
 	public void setup() {
 		background(0);
 		noStroke();
-		ball = new Circle(this, new Vector(width / 2, 50), new Vector(100,100));
-		gravity = 0.1f;
 	}
 	@Override
 	public void settings() {
@@ -21,10 +18,5 @@ public class Platformer extends PApplet{
 	@Override
 	public void draw() {
 		background(0);
-		ball.draw();
-		ball.velocity+= gravity;
-		ball.update();
-		if (ball.position.y + 50 > height)
-			ball.velocity*= -1;
 	}
 }
