d991a489424d449059a5c208dd0fd072d7c8cd47
DoubleA <DoubleA@DESKTOP-UD52PQS.fios-router.home>
2019-01-04 16:54:00 -0500

    Cleared up program

--- Platformer/src/Platformer.java
+++ Platformer/src/Platformer.java
@@ -2,62 +2,29 @@ import backgroundUtils.Circle;
 import backgroundUtils.Vector;
 import processing.core.*;
 public class Platformer extends PApplet{
+	private Circle ball;
+	private double gravity;
 	public static void main(String[] args) {
 		PApplet.main("Platformer");
 	}
-	private Circle circle;
-	public Vector degree;
-	private Vector radius;
-	Vector center;
-	Color color;
+	@Override
 	public void setup() {
 		background(0);
 		noStroke();
-		circle = new Circle(this, new Vector(200, 200), new Vector(5,5));
-		center = new Vector(width/2, height / 2);
-		degree = new Vector(50,0, -0.01); // initial value, speed, acceleration
-		radius = new Vector(0,1, 0); // initial value, speed, acceleration;
-		color = new Color(new Vector(255,0,-0.01),new Vector(34,0,0.01),new Vector(77,0,-0.01));	
+		ball = new Circle(this, new Vector(width / 2, 50), new Vector(100,100));
+		gravity = 0.1f;
 	}
+	@Override
 	public void settings() {
-		  size(1920, 1080);
+		  size(1920, 900);
 	}
 	@Override
 	public void draw() {
-		fill(color.red.x,color.green.x,color.blue.x);
-		circle.position.x = (int)(cos(degree.x) * radius.x + center.x);
-		circle.position.y = (int)(sin(degree.x) * radius.x + center.y);
-		radius.changeVector();
-		degree.changeVector();
-		color.changeColor();
-		color.bounceBack();
-		circle.draw();
-	}
-	class Color {
-		Vector red; // Saves red
-		Vector green; // Saves green
-		Vector blue; // Saves blue
-		public Color (Vector red, Vector green, Vector blue) {
-			this.red = red;
-			this.green = green;
-			this.blue = blue;
-		}
-		void changeColor() {
-			red.changeVector();
-			green.changeVector();
-			blue.changeVector();
-		}
-		void bounceBack() {
-			bounceBack(red);
-			bounceBack(green);
-			bounceBack(blue);
-		}
-		private void bounceBack(Vector value) {
-			if (value.z != 0 && (value.x > 255 && value.z > 0 || value.x < 0 && value.z < 0)) {
-				value.z *= -1;
-			}else if (value.x > 255 && value.y > 0 || value.x < 0 && value.y < 0) {
-				value.y *= -1;
-			}
-		}
+		background(0);
+		ball.draw();
+		ball.velocity+= gravity;
+		ball.update();
+		if (ball.position.y + 50 > height)
+			ball.velocity*= -1;
 	}
 }
--- Platformer/src/backgroundUtils/Circle.java
+++ Platformer/src/backgroundUtils/Circle.java
@@ -4,12 +4,17 @@ public class Circle {
 	PApplet parent;
 	public Vector position;
 	public Vector size;
+	public float velocity;
 	public Circle(PApplet parent, Vector position, Vector size) {
 		this.parent = parent;
 		this.position = position;
 		this.size = size;
+		velocity = 0;
 	}
 	public void draw() {
 		parent.ellipse(position.x,position.y,size.x,size.y);
+	}
+	public void update() {
+		this.position.y += this.velocity;
 	}
 }
