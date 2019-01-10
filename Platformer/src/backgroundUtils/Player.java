8a6cfc9eb5e6071409ec82064dc176246eb8eb2d
ahmedhhw <ahmedhhw@gmail.com>
2019-01-09 20:54:32 -0500

    Added more comments

--- Platformer/src/backgroundUtils/Player.java
+++ Platformer/src/backgroundUtils/Player.java
@@ -19,7 +19,13 @@ public class Player {
 	private Vector velocity;
 	private float gravity;
 	
-	//Constructor
+	/**
+	 * Constructor
+	 * @param parent
+	 * @param position
+	 * @param size
+	 * @param gravity
+	 */
 	public Player(PApplet parent, Vector position, Vector size, float gravity) {
 		this.parent = parent;
 		this.gravity = gravity;
@@ -29,7 +35,9 @@ public class Player {
 		velocity = new Vector(5, 0);
 	}
 	
-	//Handles everything player related
+	/**
+	 * Handles everything player related
+	 */
 	public void handleEverything() {
 		
 		//Set up controls
@@ -38,6 +46,10 @@ public class Player {
 		//Handles physics
 		handlePhysics();
 	}
+	
+	/**
+	 * Handles physics
+	 */
 	private void handlePhysics() {
 		if (playerIsOnGround()) {
 			resetAndBringUp();
@@ -47,28 +59,52 @@ public class Player {
 		handlePositionUpdate();
 	}
 
+	/**
+	 * Once player falls on a hard surface it stops falling
+	 */
 	private void resetAndBringUp() {
-	
 		this.velocity.y = 0;
+		
+		//Adjust player to correct location
 		this.hitbox.position.y = parent.height - this.hitbox.size.y; 
 	}
+	
+	/**
+	 * Checks if player is on ground
+	 * @return
+	 */
 	private boolean playerIsOnGround() {
 		return this.hitbox.position.y + this.hitbox.size.y >= parent.height;
 	}
+	/**
+	 * Updates players new location
+	 */
 	private void handlePositionUpdate() {
 		//System.out.println("Position of player:" + this.position.y);
 		//System.out.println("Velocity of player:" + this.velocity.y);
 		this.hitbox.position.y += this.velocity.y;
 		this.hitbox.position.z += this.velocity.y;
 	}
+	
+	/**
+	 * Adds gravity to velocity
+	 */
 	private void handleGravity() {
 		this.velocity.y += this.gravity;
 	}
+	
+	/**
+	 * Handles controls
+	 */
 	public void handleTheControls() {
 		handleJumping();
 		handleMoving();
 		handleDucking();
 	}
+	
+	/**
+	 * Keeps player height at half while s is held and player is not moving
+	 */
 	private void handleDucking() {
 		if (parent.key == 's' && parent.keyPressed) {
 			this.hitbox.size.y = this.hitbox.size.z / 2;
@@ -78,6 +114,10 @@ public class Player {
 			this.hitbox.position.y = this.hitbox.position.z;
 		}
 	}
+	
+	/**
+	 *  Handles moving
+	 */
 	private void handleMoving() {
 		if (parent.keyPressed) {
 			if (parent.key == 'a') {
@@ -87,6 +127,9 @@ public class Player {
 			}
 		}
 	}
+	/**
+	 * Gives player initial speed if he is on ground and w is pressed
+	 */
 	private void handleJumping() {
 		if (parent.keyPressed && parent.key == 'w' && playerIsOnGround()) {
 			this.hitbox.position.y -= 1;
@@ -94,6 +137,9 @@ public class Player {
 			this.velocity.y = -10;
 		}
 	}
+	/**
+	 * 
+	 */
 	public void draw(){
 		hitbox.draw();
 	}
